46c54b9ddb5fa310e056f23ced012b05b85ec3d9
Thomas Duerlund <td@onlinecity.dk>
2020-01-24 23:50:39 +0100

    User can subscribe to push notification topics

--- app/src/main/java/dk/lysesort/chitchat/MainActivity.java
+++ app/src/main/java/dk/lysesort/chitchat/MainActivity.java
@@ -1,10 +1,12 @@
 package dk.lysesort.chitchat;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         FirebaseApp.getInstance();
+
+        FirebaseInstanceId.getInstance()
+            .getInstanceId()
+            .addOnSuccessListener(instanceIdResult -> {
+                String token = instanceIdResult.getToken();
+                Log.d("FCM", "InstanceId: " + token);
+            });
 
         try {
             Thread.sleep(1000);
