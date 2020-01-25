f5eb08ad112936639994a64f70e05ce98fe13d69
Thomas Duerlund <td@onlinecity.dk>
2020-01-25 23:51:44 +0100

    Refactor

--- app/src/main/java/dk/lysesort/chitchat/chatroom/ChatRoomPreferenceRepository.java
+++ app/src/main/java/dk/lysesort/chitchat/chatroom/ChatRoomPreferenceRepository.java
@@ -2,6 +2,12 @@ package dk.lysesort.chitchat.chatroom;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import dk.lysesort.chitchat.R;
 
 public class ChatRoomPreferenceRepository {
 
@@ -9,45 +15,63 @@ public class ChatRoomPreferenceRepository {
 
     }
 
-    private static SharedPreferences getSharedPreferences(Context context) {
-        return context.getSharedPreferences("subscriptions", Context.MODE_PRIVATE);
-    }
-
     public static boolean hasAnswered(Context context, String chatRoomId) {
         return getSharedPreferences(context).contains(chatRoomId);
+    }
+
+    private static SharedPreferences getSharedPreferences(Context context) {
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String name = "subscriptions_" + uid;
+        return context.getSharedPreferences(name, Context.MODE_PRIVATE);
+    }
+
+    /**
+     * Toggle subscription state and return the new value.
+     *
+     * @param context    context for loading shared preferences
+     * @param chatRoomId chat room id
+     *
+     * @return Toggled value
+     */
+    public static boolean toggleSubscription(Context context, String chatRoomId) {
+        boolean subscribed = isSubscribedToPush(context, chatRoomId);
+
+        if (subscribed) {
+            unsubscribe(context, chatRoomId);
+        } else {
+            subscribe(context, chatRoomId);
+        }
+
+        return !subscribed;
     }
 
     public static boolean isSubscribedToPush(Context context, String chatRoomId) {
         return getSharedPreferences(context).getBoolean(chatRoomId, false);
     }
 
-    /**
-     * Toggle subscription state and return the new value.
-     * @param context context for loading shared preferences
-     * @param chatRoomId chat room id
-     * @return Toggled value
-     */
-    public static boolean toggleSubscription(Context context, String chatRoomId) {
-        boolean subscribed = isSubscribedToPush(context, chatRoomId);
-        getSharedPreferences(context)
-            .edit()
-            .putBoolean(chatRoomId, !subscribed)
-            .apply();
-
-        return !subscribed;
+    public static void subscribe(Context context, String chatRoomId) {
+        FirebaseMessaging.getInstance().subscribeToTopic(getTopic(chatRoomId))
+            .addOnSuccessListener(aVoid -> setSubscription(context, chatRoomId, true));
     }
 
-    public static void subscribe(Context context, String chatRoomId) {
+    private static String getTopic(String chatRoomId) {
+        return "chatroom." + chatRoomId;
+    }
+
+    private static void setSubscription(Context context, String chatRoomId, boolean subscribe) {
         getSharedPreferences(context)
             .edit()
-            .putBoolean(chatRoomId, true)
+            .putBoolean(chatRoomId, subscribe)
             .apply();
+
+        Toast.makeText(context,
+                       subscribe ? R.string.notification_toast_subscribed
+                                 : R.string.notification_toast_unsubscribed,
+                       Toast.LENGTH_SHORT).show();
     }
 
     public static void unsubscribe(Context context, String chatRoomId) {
-        getSharedPreferences(context)
-            .edit()
-            .putBoolean(chatRoomId, false)
-            .apply();
+        FirebaseMessaging.getInstance().unsubscribeFromTopic(getTopic(chatRoomId))
+            .addOnSuccessListener(aVoid -> setSubscription(context, chatRoomId, false));
     }
 }
--- app/src/main/java/dk/lysesort/chitchat/chatroom/ChatRoomViewModel.java
+++ app/src/main/java/dk/lysesort/chitchat/chatroom/ChatRoomViewModel.java
@@ -1,16 +1,21 @@
 package dk.lysesort.chitchat.chatroom;
 
+import android.app.AlertDialog;
+import android.content.Context;
+
 import com.google.firebase.storage.StorageReference;
 
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModel;
+import dk.lysesort.chitchat.R;
 
 public class ChatRoomViewModel extends ViewModel {
-    public static final String TAG = "Message";
+    private String chatRoomId;
     private ChatMessageRepository repository;
     private ChatMessageAdapter adapter;
 
     public ChatRoomViewModel(String chatRoomId) {
+        this.chatRoomId = chatRoomId;
         repository = new ChatMessageRepository(chatRoomId);
         adapter = new ChatMessageAdapter(repository);
     }
@@ -40,6 +45,37 @@ public class ChatRoomViewModel extends ViewModel {
 
     public void sendMessage(StorageReference storageReference) {
         repository.sendMessage(storageReference);
+    }
+
+    /**
+     * Displays a dialog asking the user if he/she want to receive push notifications for this chat
+     * room.
+     * <p>
+     * It is only displayed if it is the first time the user is in the chat room.
+     *
+     * @param context Context for the dialog
+     */
+    public void showPushNotificationDialog(Context context) {
+        if (ChatRoomPreferenceRepository.hasAnswered(context, chatRoomId)) {
+            return;
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage(R.string.notification_dialog_message)
+            .setPositiveButton(
+                R.string.notification_dialog_button_positive,
+                (dialog, which) -> ChatRoomPreferenceRepository.subscribe(context, chatRoomId)
+            )
+            .setNegativeButton(
+                R.string.notification_dialog_button_negative,
+                (dialog, which) -> ChatRoomPreferenceRepository.unsubscribe(context, chatRoomId)
+            )
+            .create()
+            .show();
+    }
+
+    public boolean onToggleNotifications(Context context) {
+        return ChatRoomPreferenceRepository.toggleSubscription(context, chatRoomId);
     }
 
     public ChatMessageAdapter getAdapter() {
